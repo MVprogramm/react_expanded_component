2e14a327f806a1beb22c0b592a7a2b46877ab4ce
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-06-23 10:02:55 +0300

    fix components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
 import React from "react";
 import Expand from "./Expand.jsx";
 
-class App extends React.Component {
-  state = {
-    isExpand: false
-  }
-
-handleContent = () => {
-    this.setState({
-      isExpand: !this.state.isExpand
-    })
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Expand 
-          isExpand={this.state.isExpand} 
-          title="About Hooks"
-          onClose={this.handleContent}
-        >
-          <p>
-            Hooks are a new addition in React 16.8. 
-            They let you use state and other React 
-            features without writing a class.
-          </p>
-        </Expand>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="app">
+      <Expand 
+        title="About Hooks"
+      >
+        <p>
+          Hooks are a new addition in React 16.8. 
+          They let you use state and other React 
+          features without writing a class.
+        </p>
+      </Expand>
+    </div>
+  );
 };
 
 export default App;
--- src/Expand.jsx
+++ src/Expand.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 
-const Expand = ({ isExpand, children, title, onClose }) => {
-  let expandContent = null;
-  let arrowDirection = "▼";
-    if (isExpand) {
-      expandContent = children;
-      arrowDirection = "▲";
-    }
-  
-  return (
-    <div className="expand border">
-    <div className="expand__header">
-      <span className="expand__title">{title}</span>
-      <button 
-        className="expand__toggle-btn"
-        onClick={onClose}
-      >
-        {arrowDirection}
-      </button>
+class Expand extends React.Component {
+  state = {
+    isExpand: false
+  }
+
+handleContent = () => {
+    this.setState({
+      isExpand: !this.state.isExpand
+    })
+  }
+
+  render() {
+    const { children, title } = this.props;
+
+    let expandContent = null;
+    let arrowDirection = "▼";
+      if (this.state.isExpand) {
+        expandContent = children;
+        arrowDirection = "▲";
+      };
+
+    return (
+      <div className="expand border">
+      <div className="expand__header">
+        <span className="expand__title">{title}</span>
+        <button 
+          className="expand__toggle-btn"
+          onClick={this.handleContent}
+        >
+          {arrowDirection}
+        </button>
+      </div>
+      <div className="expand__content">
+        {expandContent}
+      </div>
     </div>
-     <div className="expand__content">
-      {expandContent}
-    </div>
-  </div>
   );
+  }
 }
 
 export default Expand;
